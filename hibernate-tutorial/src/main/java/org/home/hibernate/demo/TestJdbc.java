c17de8ded0ad82cf5c12058178fb5bcfd229b37e
Dima Maloshtan <maloshtanda@gmail.com>
2021-05-13 19:48:14 +0300

    Lesson 20 "Hibernate cofiguration with Annotations"

---
+++ hibernate-tutorial/src/main/java/org/home/hibernate/demo/TestJdbc.java
@@ -0,0 +1,26 @@
+package org.home.hibernate.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try{
+            System.out.println("Connecting to Database: " + jdbcUrl);
+            Connection con = DriverManager.getConnection(jdbcUrl,user,pass);
+            System.out.println("Connecting successful");
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println(Byte.MAX_VALUE);
+
+    }
+}
