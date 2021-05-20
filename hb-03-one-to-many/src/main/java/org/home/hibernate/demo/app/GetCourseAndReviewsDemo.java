172e6b60ed3ec99f6d8eddd70711014b31c4a463
Dima Maloshtan <maloshtanda@gmail.com>
2021-05-20 23:19:19 +0300

    Lesson 26 "Hibernate Advanced Mappings - @OneToMany - Unidirectional"

---
+++ hb-03-one-to-many/src/main/java/org/home/hibernate/demo/app/GetCourseAndReviewsDemo.java
@@ -0,0 +1,42 @@
+package org.home.hibernate.demo.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.home.hibernate.demo.entity.Course;
+import org.home.hibernate.demo.entity.Instructor;
+import org.home.hibernate.demo.entity.InstructorDetail;
+import org.home.hibernate.demo.entity.Review;
+
+public class GetCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            int idCourse = 10;
+
+            session.beginTransaction();
+
+
+            Course course = session.get(Course.class, idCourse);
+            System.out.println("\n" + course.getReviews());
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
