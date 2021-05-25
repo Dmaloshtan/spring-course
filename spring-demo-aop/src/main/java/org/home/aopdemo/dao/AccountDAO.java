2dc980fb24a80306137b047f42dade1153b04310
Dima Maloshtan <maloshtanda@gmail.com>
2021-05-25 22:21:29 +0300

    Lesson 35 "AOP @Before Advice Type"

---
+++ spring-demo-aop/src/main/java/org/home/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,12 @@
+package org.home.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount(){
+
+        System.out.println(getClass() + ": doing my db work: adding an account");
+    }
+}
